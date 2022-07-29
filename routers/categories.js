a07b2c66dfbdd23ac2962b4910a499eb2f179513
aswin <aswinpalayad@ymail.com>
2022-07-29 14:58:25 +0530

    july 29,getCategory

--- routers/categories.js
+++ routers/categories.js
@@ -10,8 +10,18 @@ router.get('/',async (req,res)=>{
         res.status(500).json({success:false})
     }
 
-    res.send(categoryList)
+    res.status(200).send(categoryList)
 
+})
+
+router.get('/:id',async (req,res)=>{
+    const category = await Category.findById(req.params.id)
+
+    if(!category){
+        res.status(500).json({message:'The category with the given Id was not found'})
+    }
+
+    res.status(200).send(category)
 })
 
 router.post('/',async (req,res)=>{
