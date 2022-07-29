dc0dda0bd34d08dc7b4a7133562510ce925a1340
aswin <aswinpalayad@ymail.com>
2022-07-29 10:44:45 +0530

    july29

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express();
 const morgan = require('morgan')
 const mongoose = require('mongoose');
-const { json } = require('express');
-const Product = require('./models/product')
+const cors = require('cors');
+
+// const { json } = require('express');
+// const Product = require('./models/product')
+
 require('dotenv/config')
 
+//cors 
+app.use(cors());
+app.options('*',cors())
 
 //middleware
 app.use(express.json())
@@ -15,13 +21,17 @@ app.use(morgan('tiny'))
 //Routes
 const productRouter = require('./routers/products')
 const userRouter = require('./routers/users')
+const orderRouter = require('./routers/orders')
+const categoryRouter = require('./routers/categories')
 
 
 const api = process.env.API_URL;
 
 
-app.use(`${api}/product`,productRouter)
+app.use(`${api}/products`,productRouter)
 app.use(`${api}/users`,userRouter)
+app.use(`${api}/orders`,orderRouter)
+app.use(`${api}/categories`,categoryRouter)
 
 
 
