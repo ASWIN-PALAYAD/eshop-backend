115415c4eb0f6cd70067e85440d11cfdf1cf62a5
aswin <aswinpalayad@ymail.com>
2022-08-02 12:52:20 +0530

    august2 order

---
+++ models/order-item.js
@@ -0,0 +1,15 @@
+const { TokenExpiredError } = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+const orderItemSchema = mongoose.Schema({
+    quantity:{
+        type : Number,
+        required :true
+    },
+    product:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref:'Product'
+    }
+})
+
+exports.OrderItem = mongoose.model('orderItem',orderItemSchema)
